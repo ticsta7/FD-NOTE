160470d260e44cd67983e90b7b270f1c3e8b26d2
ticsta7 <ticsta7@163.com>
2019-09-08 21:48:41 +0800

    oop

--- js基础/oop.js
+++ js基础/oop.js
@@ -1,7 +1,5 @@
 console.clear();
 
-// 定义类，之后可以直接new ，
-
 class salaryData  
 {
   constructor( city , position , salary_max , salary_min )
@@ -12,18 +10,47 @@ class salaryData
     this.salary_max = salary_max;
     this.salary_min = salary_min; 
   }
-  
   //这个是方法
   avg_salary()
   {
-      console.log((this.salary_min + this.salary_max)/2);
-      
+       return (this.salary_max + this.salary_min)/2;
   }
-  
+
+  pure_income()
+  {
+    return this.avg_salary()*0.6;
+  }
 }
 
-var gz_fe = new salaryData( 'gz' , 'fe' , 18000 , 8000 );
+class capitalSalaryData extends salaryData
+{  //派生出一个子类
+  constructor( city , position , salary_max , salary_min )
+  {  //从super继承来
+    super( city , position , salary_max , salary_min );
+  }
+  
+  pure_income()//覆盖之前的方法
+  {
+    return this.avg_salary()*0.55;
+  }
+}
+
+var test =[];
+// 依然 new salaryData
+test.push( new salaryData('gz','fe',18000,8000));
+
+// capitalSalaryData
+test.push( new capitalSalaryData( 'beijing' , 'fe' , 20000 , 10000 ) );
+
+for( i in test)
+{
+    console.log(test[i].pure_income());
+}
+
+ //分割线
 
 
 
-gz_fe.avg_salary();
+
+
+
